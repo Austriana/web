664fc7febbd529a34dbede595591608dcd9ca8c4
Austriana <Marco.Mang@gmx.de>
2025-08-12 22:56:14 +0200

    test

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let fetch_data = async (token) => {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
-    }
+    };
     if(response.ok){
         const result = await response.json();
         if(token === result.token){
             window.location.replace(result.path);
-        }
-    }
+        };
+    };
   } catch (error) {
     console.error(error.message);
-  }
-}
+  };
+};
 
 let submit = document.getElementById('submit');
 submit.addEventListener('click', () => {
